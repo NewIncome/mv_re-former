02c26bff599fba32ce1f991e63bc59a7279d5663
Yosept <yosept.flores@gmail.com>
2020-02-12 15:34:26 -0600

    We added a form, wich sends a get to the users, and makes posibble to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: { params: params, status: 'saved' }, status: :ok
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                 :password_confirmation)
   end
 end
